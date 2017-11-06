c2acca2ff11d4e0f3b3d50d3e09fd40dab99cd1c
barhom1307 <33415520+barhom1307@users.noreply.github.com>
2017-11-06 11:10:25 +0200

    Add files via upload

---
+++ get_LiDAR_data.py
@@ -0,0 +1,54 @@
+import socket
+from control_panel_function import send_data_UDP 
+from decryptor import decryptor
+import os
+import time
+def get_LiDAR_data(IP_LiDAR,port_LiDAR,V_matlab,IP_matlab,port_matlab,namber_of_packeges,save_path,file_name,file_nunber):
+    """
+    function call:
+    get_LiDAR_data(IP_LiDAR,port_LiDAR,V_matlab,IP_matlab,port_matlab,...
+    namber_of_packeges,save_path,file_name,file_nunber):
+        
+    function return:
+    NONE. The function creates a text file with the recived data.
+    
+    what dose the function:
+    the function recives data from the LiDAR sensor via the rauter,
+    decreypt it and creates a text file with the decrypted data. This file is leater 
+    being read by the matlab program. 
+    """
+    time.sleep(1)
+    # creating connection between the computer and the LiDAR:
+    sock = socket.socket(socket.AF_INET,            # Internet
+                              socket.SOCK_DGRAM)        # UDP
+    sock.bind((IP_LiDAR,port_LiDAR))    
+        
+    # get the data from the lidar:
+    data=[]
+    for i in range(0,namber_of_packeges):
+        packet, addr = sock.recvfrom(2000)
+        data.append(packet)
+            
+    # closing the socket:
+    sock.close()
+    
+    # informing that we are decrypting the data:
+    V_matlab[12]=2
+    send_data_UDP(V_matlab,IP_matlab,port_matlab)    
+      
+    # decrypting each packege:
+    decrypted_data=''
+    for i in range(0,namber_of_packeges):
+        decrypted_packet = decryptor(data[i])
+        decrypted_data   = decrypted_data + decrypted_packet
+    
+    # printing the data into text file, make sure to change the "save_path":
+    file_name = file_name + str(file_nunber) + ".txt"
+    save_name = os.path.join(save_path,file_name)
+    data_file = open(save_name, "w") 
+    data_file.write(decrypted_data)
+    data_file.close() 
+    
+    # informing that we have finished prossesing the data:
+    V_matlab[12]=3
+    send_data_UDP(V_matlab,IP_matlab,port_matlab)  
