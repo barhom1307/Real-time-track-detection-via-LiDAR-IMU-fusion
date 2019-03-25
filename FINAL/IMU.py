a801430a8e2f44cfee6a3950556e5dbe1195bb0e
barhom1307 <33415520+barhom1307@users.noreply.github.com>
2019-03-25 09:03:02 +0200

    Add files via upload
    updated IMU.py - interpolation & comments

--- FINAL/IMU.py
+++ FINAL/IMU.py
@@ -2,7 +2,6 @@
 
 from vnpy import *
 import pandas as pd
-pd.set_option('float_format', '{:f}'.format)
 import numpy as np
 from scipy.stats import linregress
 import pickle
@@ -10,18 +9,14 @@ import time
 from multiprocessing import Process, Queue
 from signal import signal, SIGTERM
 import os
-import matplotlib.pyplot as plt
 from matplotlib import style
-import matplotlib.animation as animation
-import scipy.interpolate as interp
-from mpl_toolkits.mplot3d import axes3d
-
-import RotMatrixes
-import utilities
+from pytictoc import TicToc
+from orderedset import OrderedSet
+pd.set_option('float_format', '{:f}'.format)
 style.use('fivethirtyeight')
+timer = TicToc()
 
-NUM_OF_PACKETS_LIDAR = 754  # number of lidar packets per 1 sec
-NUM_OF_PACKETS_IMU = 20     # number of IMU packets per 1 sec
+'''User Parameters'''
 COLUMNS_IMU = 7             # number of IMU data columns
 FLAG = True
 
@@ -30,18 +25,23 @@ def handler(signum, frame):
     global FLAG
     FLAG = False
 
+'''   
+Abstract: Reads data live from IMU sensor
 
+Parameters: full path to pickle (for option to save data)
+
+Returns:
+'''
 class vn200_online_feeder:
     def __init__(self, imu_pkl):
         self.IMU_PKL = imu_pkl
         self.run()
 
-
     def receiver(self, queue):
         sensor = VnSensor()
         print('Establishing Connection with IMU...')
         sensor.connect('/dev/ttyUSB0', 115200)  # Default = 115200, fastest 921600
-        sensor.change_baudrate(921600)
+        # sensor.change_baudrate(921600)
         print('Connection Established, ' + 'Baudrate is: ' + str(sensor.read_serial_baudrate()))
         signal(SIGTERM, handler)
         self.imu_packets = []
@@ -58,7 +58,6 @@ class vn200_online_feeder:
             ]
             queue.put(lla_data)
             self.imu_packets.append(lla_data)
-        sensor.change_baudrate(115200)
         with open(self.IMU_PKL, 'wb') as imu_f:
             pickle.dump(self.imu_packets, imu_f)
         os._exit(0)
@@ -72,9 +71,17 @@ class vn200_online_feeder:
         return self.queue.get()
 
     def close_imu(self):
-
         print('Terminating IMU Connection...')
         self.proc.terminate()
+
+
+'''   
+Abstract: Reads IMU data from csv file
+
+Parameters: imu_pkl - full path to pickle file of the imu data. (pickle  = serialaized data)
+
+Returns: 
+'''
 
 
 class vn200_offline_feeder:
@@ -91,31 +98,53 @@ class vn200_offline_feeder:
                     break
         imu_array = np.reshape(imu_array, (int(len(imu_array)/COLUMNS_IMU), COLUMNS_IMU))
         self.imu_list = imu_array.tolist()
-        #print(self.imu_list)
 
     def get_packet(self):
         return self.imu_list.pop(0)
 
+'''
+Abstract: Changes frame of refrence of lidar X-Y-Z
+          to Earth LLA frame of refrence using IMU data, Y-P-R.
+          
+Parameters: X-Y-Z-t - data points filtered as cones by the cone_finder function
 
-fig = plt.figure()
-ax1 = fig.add_subplot(111, projection='3d')
+Returns: X-Y-Z in Earth frame of refrence
+'''
 
 
 class vn200_decoder:
-
     def __init__(self, packet_feeder):
         self.packet_feeder = packet_feeder
 
+    '''   
+    Abstract: Finds the IMU values within an error value (default is 0.1 sec) to the times in lidar frame
+
+    Parameters: list of lidar of all packet times in lidar frame
+
+    Returns: IMU values closest to packets time (with error, default 0.1 sec)
+    '''
     def get_imu_frame(self, lidar_frame_time):
         imu_t1 = self.packet_feeder.get_packet()
-        while abs(imu_t1[6] - lidar_frame_time[0]) > 0.01:
-            imu_t1 = self.packet_feeder.get_packet()
+        try:
+            while abs(imu_t1[6] - lidar_frame_time[0]) > 0.1:  # TODO:Print and check times
+                imu_t1 = self.packet_feeder.get_packet()
+        except KeyError as e:
+            pass
         imu_t2 = self.packet_feeder.get_packet()
         return [imu_t1, imu_t2]
 
-    # [1, 2, 3, 4, 5, 6, 0], [2, 3, 4, 5, 6, 7, 1], [-1, 1, 100])
-    def imu_interpolate(self, imu_data1, imu_data2, lidar_frame_time):  # 1x7, 1x7, 1xlen(lidar_time)) || LIDAR_XYZT[3]
-        self.interp_imu_frame = pd.DataFrame(np.zeros((len(lidar_frame_time), 7)))
+    '''   
+    Abstract: Uses linear regression to find the linear equeation of each imu data point. 
+              Assumes change is linear around a small time delta. (For better precision different implementation maybe needed)
+
+    Parameters: imu_data1 - closest data point before the lidar packet time point
+                imu_data2 - closest data point after the lidar packet time point
+                lidar_packet_time - time of all XYZ in this packet
+
+    Returns: saves a 2x6 array with the slope (first row) and intercept (second row) for each IMU data, Y-P-R-L-L-A.
+    '''
+    def imu_interpolate(self, imu_data1, imu_data2, lidar_packet_time):  # 1x7, 1x7, 1xlen(lidar_time)) || LIDAR_XYZT[3]
+        self.interp_imu_frame = pd.DataFrame(np.zeros((len(lidar_packet_time), 7)))
         imu_data = pd.DataFrame([imu_data1[0:6], imu_data2[0:6]])  # 2x6, without time values
         time_vec = [imu_data1[6], imu_data2[6]]
 
@@ -124,12 +153,19 @@ class vn200_decoder:
 
         imu_data_intercept = imu_data.apply(lambda y: linregress(time_vec, y).intercept, axis=0)
         # .apply passes each of the columns of imu_data (shape: [2x1]) to y.
-
         linear_df = pd.DataFrame([imu_data_slope, imu_data_intercept])
-        for i, t in enumerate(lidar_frame_time):
+        for i, t in enumerate(lidar_packet_time):
             self.interp_imu_frame.iloc[i, :] = linear_df.apply(lambda n: n[0] * t + n[1], axis=0)
-        self.interp_imu_frame[6] = lidar_frame_time
+        self.interp_imu_frame[6] = lidar_packet_time
 
+    '''   
+    Abstract: Creates 3 rotation matrixs 1 for each axis using the yaw, pitch and roll 
+              from the IMU and combines them to 1 rotation matrix.
+
+    Parameters: ypr - yaw, pitch, roll for the specific time we want.
+
+    Returns: rotation matrix around all 3 axis
+    '''
     def rotation_matrix(self, ypr):
         yaw = ypr[0]
         pitch = ypr[1]
@@ -149,76 +185,25 @@ class vn200_decoder:
 
         return R_Mat_Yaw.dot(R_Mat_Pitch.dot(R_Mat_Roll))  # Returns matrix for LLA frame of reference
 
+    '''   
+    Abstract: The main function of the class, recieves lidar points in 
+              lidar frame of refrence (FoR) and returns them in Earch LLA FoR
+
+    Parameters: xyzt_cones - lidar points with time stamp after cone filtering
+
+    Returns: xyz in earth LLA FoR
+    '''
     def get_world_coords(self, xyzt_cones):  # both inputs are arranged so each row corresponds to the same time.
+        lidar_time_series = pd.Series(OrderedSet(xyzt_cones.iloc[:, 3]))
         xyzt_cones = pd.DataFrame(xyzt_cones)
-        imu_t1, imu_t2 = vn200_decoder.get_imu_frame(self, lidar_frame_time=xyzt_cones.iloc[:, 3])
-        pd.DataFrame(vn200_decoder.imu_interpolate(self, imu_t1, imu_t2, lidar_frame_time=xyzt_cones.iloc[:, 3]))
+        xyz_cones_imu_coord_sys = xyzt_cones.iloc[:, 0:3].dot(np.mat([[0, 1, 0], [1, 0, 0],  [0, 0, -1]]))
+        imu_t1, imu_t2 = vn200_decoder.get_imu_frame(self, lidar_frame_time=lidar_time_series)
+        vn200_decoder.imu_interpolate(self, imu_t1, imu_t2, lidar_frame_time=lidar_time_series)
         ypr_db = self.interp_imu_frame.iloc[:, 0:3]
-        ypr_rtm = [ypr_db.apply(lambda ypr: vn200_decoder.rotation_matrix(self, ypr), axis=1)]
-
-        return xyzt_cones.iloc[:, 0:3].dot(ypr_rtm[0][0])
-
-    def plot_3d(self, figure, xyz_frame, points=0.5):
-        axes_str = ['X', 'Y', 'Z']
-        axes_limits = [
-            [-5, 5],  # X axis range
-            [0, 20],  # Y axis range
-            [-0.2, 0.6]  # Z axis range
-        ]
-
-        point_size = 0.01 * (1. / points)
-
-        def draw_point_cloud(ax, title, axes=[0, 1], xlim3d=None, ylim3d=None, zlim3d=None):
-            ax.scatter(*np.transpose(xyz_frame[:, axes]), s=point_size, c=xyz_frame[:, 3], cmap='gray')
-            # ax.scatter(*np.transpose(xyz_cones[:, axes]), s=100, c='r' ,marker='^', alpha=.8)
-            ax.set_title(title, fontsize=12)
-            ax.set_xlabel('{} axis'.format(axes_str[axes[0]]))
-            ax.set_ylabel('{} axis'.format(axes_str[axes[1]]))
-            if len(axes) > 2:
-                ax.set_xlim3d(*axes_limits[axes[0]])
-                ax.set_ylim3d(*axes_limits[axes[1]])
-                ax.set_zlim3d(*axes_limits[axes[2]])
-                ax.set_zlabel('{} axis'.format(axes_str[axes[2]]))
-            else:
-                ax.set_xlim(*axes_limits[axes[0]])
-                ax.set_ylim(*axes_limits[axes[1]])
-            # User specified limits
-            if xlim3d != None:
-                ax.set_xlim3d(xlim3d)
-            if ylim3d != None:
-                ax.set_ylim3d(ylim3d)
-            if zlim3d != None:
-                ax.set_zlim3d(zlim3d)
-
-        ax=figure.add_subplot(111)
-        plt.gca()
-        draw_point_cloud(ax, 'Velodyne scan, XY projection (Z = 0), the car is moving forward (160 deg view)', axes=[0, 1])
-        ax.set_aspect('equal')
-        plt.draw()
-        plt.pause(0.001)
-        plt.clf()
-
-    # def animate(i):
-    #     ax1 = fig.add_subplot(111, projection='3d')
-    #     ax1.clear()
-    #
-    #     xyz = vn200_decoder.get_world_coords(self, xyzt_cones)
-    #
-    #     ax1.set_xlim3d([0, 1])
-    #     ax1.set_ylim3d([-1, 1])
-    #     ax1.set_zlim3d([-1, 1])
-    #     ax1.set_title('LIDAR', fontsize=12)
-    #     ax1.set_xlabel('{} axis'.format('X'))
-    #     ax1.set_ylabel('{} axis'.format('Y'))
-    #     ax1.set_zlabel('{} axis'.format('Z'))
-    #     ax1.view_init(0, 0)
-    #     utilities.set_aspect_equal_3d(ax1)
-    #     ax1.scatter(xyz[:, 0], xyz[:, 1], xyz[:, 2], s=10)
-    #
-    # def online_feed(self, xyzt_cones):
-    #     fig = plt.figure()
-    #     ani = animation.FuncAnimation(fig, vn200_decoder.animate, interval=10)  # Live ploting
-    #     plt.show()
+        ypr_rotation_mat = vn200_decoder.rotation_matrix(self, ypr_db.iloc[round(len(ypr_db)/2), :])
+        xyz_cones_imu_coord_sys = xyz_cones_imu_coord_sys.dot(ypr_rotation_mat)
+        xyz_cones_lidar_coord_sys = xyz_cones_imu_coord_sys.dot(np.mat([[1, 0, 0], [0, -1, 0], [0, 0, -1]])), ypr_db.iloc[round(len(ypr_db) / 2), :]
+        return xyz_cones_lidar_coord_sys
 
 
 def main():
